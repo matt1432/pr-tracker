8efa8e09ecfbda13e0b48996569235ae0a79ebd0
Alyssa Ross <hi@alyssa.is>
2024-07-29 10:06:49 +0200

    nixpkgs: give candidates to git branch --contains
    This should make the git processes a lot more efficient, because they
    only have to check the branches we're actually interested in, not
    every branch in the repo.

--- src/tree.rs
+++ src/tree.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later WITH GPL-3.0-linking-exception
-// SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
+// SPDX-FileCopyrightText: 2021, 2024 Alyssa Ross <hi@alyssa.is>
 // SPDX-FileCopyrightText: 2022 Arnout Engelen <arnout@bzzt.net>
 
 use std::collections::BTreeSet;
@@ -40,6 +40,13 @@ impl Tree {
         }
     }
 
+    fn all_branches<'s: 'o, 'o>(&'s self, out: &mut BTreeSet<&'o str>) {
+        out.insert(&self.branch_name);
+        for child in &self.children {
+            child.all_branches(out);
+        }
+    }
+
     fn fill_accepted(&mut self, branches: &BTreeSet<OsString>, missing_means_absent: bool) {
         self.accepted = match branches.contains(OsStr::new(&self.branch_name)) {
             true => Some(true),
@@ -68,9 +75,16 @@ impl Tree {
         {
             if let Some(merge_commit) = merge_commit_oid {
                 let mut containing_commits = BTreeSet::new();
+                let mut all_branches = BTreeSet::new();
+
+                tree.all_branches(&mut all_branches);
 
                 if let Err(e) = nixpkgs
-                    .branches_containing_commit(merge_commit, &mut containing_commits)
+                    .branches_containing_commit(
+                        &all_branches,
+                        merge_commit,
+                        &mut containing_commits,
+                    )
                     .await
                 {
                     eprintln!("pr-tracker: branches_containing_commit: {}", e);
