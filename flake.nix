96f174636ac133c506606c64dcefabf57e4ee0fd
matt1432 <matt@nelim.org>
2025-08-10 15:25:00 -0400

    chore: get rid of unused nixpkgs dep

--- flake.nix
+++ flake.nix
@@ -1,33 +1,12 @@
 {
   inputs = {
-    nixpkgs = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      ref = "nixos-unstable";
-    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-
-    perSystem = attrs:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        attrs (import nixpkgs {inherit system;}));
-  in {
+  outputs = {self, ...}: {
     nixosModules = {
       pr-tracker = import ./modules/pr-tracker.nix;
 
       default = self.nixosModules.pr-tracker;
     };
-
-    formatter = perSystem (pkgs: pkgs.alejandra);
   };
 }
